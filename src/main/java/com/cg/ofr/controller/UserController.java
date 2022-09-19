dbe582cafb43af27ddb71d38ff4a6863dfb087be
Mehakpreet Kaur <mehakpreet.a.kaur@capgemini.com>
2022-09-19 17:10:32 +0530

    mehak added

--- src/main/java/com/cg/ofr/controller/UserController.java
+++ src/main/java/com/cg/ofr/controller/UserController.java
@@ -23,16 +23,16 @@ public class UserController {
 	@PutMapping("/tenant/update/user")
 	public ResponseEntity<Tenant> modifyTenant(@RequestBody Tenant tenant) {
 		Tenant updateTenant = iUserService.updateTenantPassword(tenant);
-		ResponseEntity<Tenant> responseEntity = new ResponseEntity<>(updateTenant, HttpStatus.OK);
-		return responseEntity;
+		return new ResponseEntity<>(updateTenant, HttpStatus.OK);
+		
 
 	}
 
 	@PutMapping("/landlord/update/user")
 	public ResponseEntity<Landlord> modifyLandlord(@RequestBody Landlord landlord) {
 		Landlord updateLandlord = iUserService.updateLandlordPassword(landlord);
-		ResponseEntity<Landlord> responseEntity = new ResponseEntity<>(updateLandlord, HttpStatus.OK);
-		return responseEntity;
+		return new ResponseEntity<>(updateLandlord, HttpStatus.OK);
+		
 
 	}
 
@@ -40,30 +40,30 @@ public class UserController {
 	public ResponseEntity<Object> validateTen(@PathVariable("username") String username,
 			@PathVariable("password") String password) {
 		Tenant tenant = iUserService.validateTenant(username, password);
-		ResponseEntity<Object> responseEntity = new ResponseEntity<>(tenant, HttpStatus.OK);
-		return responseEntity;
+		return new ResponseEntity<>(tenant, HttpStatus.OK);
+		
 	}
 
 	@GetMapping("/landlord/{username}/{password}")
 	public ResponseEntity<Object> validateLan(@PathVariable("username") String username,
 			@PathVariable("password") String password) {
 		Landlord landlord = iUserService.validateLandlord(username, password);
-		ResponseEntity<Object> responseEntity = new ResponseEntity<>(landlord, HttpStatus.OK);
-		return responseEntity;
+		return  new ResponseEntity<>(landlord, HttpStatus.OK);
+		
 	}
 
 	@PostMapping("/tenant/register")
 	public ResponseEntity<Object> register(@RequestBody Tenant tenant) {
 		Tenant newTenant = iUserService.validateNewTenant(tenant);
-		ResponseEntity<Object> responseEntity = new ResponseEntity<>(newTenant, HttpStatus.OK);
-		return responseEntity;
+		return new ResponseEntity<>(newTenant, HttpStatus.OK);
+		
 	}
 
 	@PostMapping("/landlord/register")
 	public ResponseEntity<Object> register(@RequestBody Landlord landlord) {
 		Landlord newLandlord = iUserService.validateNewLandlord(landlord);
-		ResponseEntity<Object> responseEntity = new ResponseEntity<>(newLandlord, HttpStatus.OK);
-		return responseEntity;
+		return  new ResponseEntity<>(newLandlord, HttpStatus.OK);
+		
 	}
 
 }
